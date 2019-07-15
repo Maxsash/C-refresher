19c6781a0436563920a407e6903a66fb2ef9b380
Maxsash <the.revived.1@gmail.com>
2019-07-15 23:38:32 +0530

    chore: Add Multi Dimensional arrays

--- pain.c
+++ pain.c
@@ -19,5 +19,20 @@ int main()
     printf("element %d = %d\n", i,NumberArray[i]);
   }
 
+  //Multi Dimensional Arrays
+  int MultiArray [2][3] = // rows, columns
+  {
+    {5,6,7}, //row 0
+    {2,7,3}  //row 1
+  };
+  //Printing a multi dimensional array
+  for (int i=0;i<2;i++)
+  {
+    for (int j=0;j<3;j++)
+    {
+      printf ("MultiArray[%d][%d]=%d\n",i,j,MultiArray[i][j]);
+    }
+  }
+
   return 0;
 }
