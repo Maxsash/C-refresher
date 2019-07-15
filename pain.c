af50e5e414c0928a0ee1577adc413e1d856dbeb6
Maxsash <the.revived.1@gmail.com>
2019-07-15 22:54:40 +0530

    chore: Add Global variable

---
+++ pain.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<stdlib.h>
+int global; //Global variable. Not inside any function.
+//Global variables are already initialized by 0. Unline local ones.
+
+int main()
+{
+  return 0;
+}
