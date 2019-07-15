b6aa33848e89cc35824a844ad45b0ee1340623d1
Maxsash <the.revived.1@gmail.com>
2019-07-15 23:28:03 +0530

    chore: Add Simple Arrays

--- pain.c
+++ pain.c
@@ -5,5 +5,19 @@ int global; //Global variable. Not inside any function.
 
 int main()
 {
+  //Arrays
+  int NumberArray [3] = {1,2,3}; //not necessary to initialize values
+  /*Here, index starts from 0
+  NumberArray[0] = 1
+  NumberArray[1] = 2
+  NumberArray[2] = 3
+  */
+  char CharArray [4] = {'a','b','c','d'}; //Defining a char array
+  //Printing an Array
+  for(int i=0;i<3;i++)
+  {
+    printf("element %d = %d\n", i,NumberArray[i]);
+  }
+
   return 0;
 }
