304d749528c2ef3c5010f317e65ab02a7a13c73c
Maxsash <the.revived.1@gmail.com>
2019-07-16 16:30:57 +0530

    chore: Add strings

--- pain.c
+++ pain.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 int global; //Global variable. Not inside any function.
 //Global variables are already initialized by 0. Unline local ones.
 
@@ -48,6 +49,25 @@ int main()
   printf("value stored at pointer = %d \n", *pointer_p);
   //There can be arrays of pointers and pointers can be arguments as well
   //Arrays can be passed as an argument by using pointers
+
+  //Strings
+  //It is single dimensional array of characters, terminated by a null charachter.
+  char my_string[6] = {'H','e','l','l','o','\0'};
+  char string1[] = "Hello";
+  //Both are the exact same.
+  printf("My string = %s\n",my_string);
+  printf("String 1 = %s\n",string1);
+
+  //the following functions are included in string.h
+  char string2[] = " world";
+  char string3[6];
+  strcpy(string3,string1); //string copy (destination, source)
+  strcat(string1,string2); // string1 = string1 + string2
+  int length= strlen(string1); //returns the length of string
+  //printing
+  printf("strcpy = %s\n",string3);
+  printf("strcat = %s\n", string1);
+  printf("strlen = %d\n", length);
 
   return 0;
 }
